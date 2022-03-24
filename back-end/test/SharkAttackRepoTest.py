8f4be6fde521415b818f71c3dfc1b2c8466772e0
sriksrik7 <sriksrik7@gmail.com>
2022-03-24 00:13:28 -0400

    Added sprint2 fixes

--- back-end/test/SharkAttackRepoTest.py
+++ back-end/test/SharkAttackRepoTest.py
@@ -1,13 +1,14 @@
 import unittest
-
+import os
 from app.SharkAttackRepo import SharkAttackRepo
 
+os.path.abspath(os.getcwd())
 
 class SharkAttackRepoTest(unittest.TestCase):
 
     def testAddAllCsv(self):
         repo = SharkAttackRepo(':memory:')
-        repo.addAllCsv('../attacks-processed.csv')
+        repo.addAllCsv(os.path.abspath("attacks-processed.csv"))
         results = repo.getAll()
         for result in results:
             print(str(result))
