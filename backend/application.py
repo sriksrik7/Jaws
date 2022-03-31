baffbdf4e9c67d56a15a9ec06af88685c9a04bc8
Tim O'Hearen <tohearen@stevens.edu>
2022-03-31 01:13:15 -0400

    move repo back

--- backend/application.py
+++ backend/application.py
@@ -2,13 +2,12 @@ from backend.attack_repo import SharkAttackRepo
 from flask import Flask, jsonify
 
 application = Flask(__name__)
-
+repo = SharkAttackRepo(':memory:')
+repo.addAllCsv('/var/app/current/backend/attacks-processed.csv')
 
 @application.route("/attacks/all", methods=['GET'])
 def getAll():
     json = []
-    repo = SharkAttackRepo(':memory:')
-    repo.addAllCsv('/var/app/current/backend/attacks-processed-sample.csv')
     for attack in repo.getAll():
         json.append(attack.serialize())
     return jsonify(json)
