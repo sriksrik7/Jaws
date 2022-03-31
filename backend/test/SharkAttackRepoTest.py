f54e32b7d50fa8845f8222636cf6d07bb9cbf282
Tim O'Hearen <tohearen@stevens.edu>
2022-03-30 22:41:27 -0400

    deploy jaws app

--- backend/test/SharkAttackRepoTest.py
+++ backend/test/SharkAttackRepoTest.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from app.SharkAttackRepo import SharkAttackRepo
+from backend.attack_repo import SharkAttackRepo
 
 os.path.abspath(os.getcwd())
 
@@ -8,7 +8,7 @@ class SharkAttackRepoTest(unittest.TestCase):
 
     def testAddAllCsv(self):
         repo = SharkAttackRepo(':memory:')
-        repo.addAllCsv(os.path.abspath("attacks-processed.csv"))
+        repo.addAllCsv(os.path.abspath("attacks-test.csv"))
         results = repo.getAll()
         for result in results:
             print(str(result))
