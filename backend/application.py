796b7bb08b5189453c8f259a2c3c7b2e78a0ab17
Tim O'Hearen <tohearen@stevens.edu>
2022-03-31 00:46:11 -0400

    tmp adjust csv loading

--- backend/application.py
+++ backend/application.py
@@ -2,13 +2,13 @@ from backend.attack_repo import SharkAttackRepo
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-repo = SharkAttackRepo(':memory:')
-repo.addAllCsv('/var/app/current/backend/attacks-processed-sample.csv')
 
 
 @app.route("/attacks/all")
 def getAll():
     json = []
+    repo = SharkAttackRepo(':memory:')
+    repo.addAllCsv('/var/app/current/backend/attacks-processed-sample.csv')
     for attack in repo.getAll():
         json.append(attack.serialize())
     return jsonify(json)
