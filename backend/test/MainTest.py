d41513a42849f2b44919adc4799388042dcad11b
Tim O'Hearen <tohearen@stevens.edu>
2022-03-31 01:29:38 -0400

    adjust tests

---
+++ backend/test/MainTest.py
@@ -0,0 +1,15 @@
+
+import unittest
+
+from test import SharkAttackRepoTest
+
+# Main Class to test all unit test classes
+
+
+class TestAll(unittest.TestCase):
+    SharkAttackRepoTest()
+
+
+if __name__ == "__main__":
+
+    unittest.main()
